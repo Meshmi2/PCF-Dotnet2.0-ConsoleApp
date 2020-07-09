720a1e263145dec5879d2fbeca539d6f27e02084
massimo trovati <massimotrovati92@gmail.com>
2020-07-09 07:56:15 +0200

    Add User Input Choice
    Add User Input Time For Loop

--- Program.cs
+++ Program.cs
@@ -6,16 +6,22 @@ namespace DOTNETCORE2
     {
         static void Main(string[] args)
         {
-             Console.WriteLine("Started Console app");
-		int i; 
-	 i = 1; 	
-            while (true) 
- 		{
+            // Initialize Console App
+            Console.WriteLine("Started Console app");
+            int i;
+            i = 1;
+            // Read User Input
+            Console.WriteLine("how often do you want the loop time?");
+            int loopvalue = Convert.ToInt32(Console.ReadLine());
+            while (true)
+            {
+                // Define Loop Time
+                System.Threading.Thread.Sleep(loopvalue);
+                // Initialize Loop
                 Console.WriteLine("Loop # {0}", i);
-		System.Threading.Thread.Sleep(300000);
                 i++;
             }
- 
+
         }
 
     }
